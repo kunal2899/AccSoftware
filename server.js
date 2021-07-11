6da3c71612717df5d7766524902fafc0b7ff5b1d
kunal2899 <kunaljain649@gmail.com>
2021-07-11 20:52:02 +0530

    ..

--- server.js
+++ server.js
@@ -15,4 +15,6 @@ res.sendFile(path.join(__dirname+'/dist/AccSoftware/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
